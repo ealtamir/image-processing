fa3198526b23c4932efa4b54298ae8cd43fba973
Federelli <federelli@gmail.com>
2015-08-26 14:17:57 -0300

    Enhance contrast bugfixes.

--- src/main/java/ar/com/itba/action/EnchanceContrastAction.java
+++ src/main/java/ar/com/itba/action/EnchanceContrastAction.java
@@ -1,5 +1,6 @@
 package ar.com.itba.action;
 
+import java.awt.Color;
 import java.awt.Component;
 import java.awt.event.ActionEvent;
 import java.awt.image.BufferedImage;
@@ -45,19 +46,35 @@ public class EnchanceContrastAction extends AbstractAction {
 		optionsPanel.add(new JLabel("enchancement: "));
 		optionsPanel.add(enhancement);
 
-		String msg = "Texto mensaje";
+		String msg = "Choose value 1, value 2 and enchancement:";
 		int result = JOptionPane.showConfirmDialog(null, optionsPanel, msg, JOptionPane.OK_CANCEL_OPTION);
 		if (result == JOptionPane.OK_OPTION) {
 			int height = image.getHeight();
 			int width = image.getWidth();
-		
+
 			for (int x = 0; x < width; x++) {
 				for (int y = 0; y < height; y++) {
-					int r = (image.getRGB(x, y))&0xFF;
-					int g = (image.getRGB(x, y)>>8)&0xFF;
-					int b = (image.getRGB(x, y)>>16)&0xFF;
-					int color = (r << 16) | (g << 8) | b;
-					image.setRGB(x, y, color);
+					int r = (image.getRGB(x, y)) & 0xFF;
+					int g = (image.getRGB(x, y) >> 8) & 0xFF;
+					int b = (image.getRGB(x, y) >> 16) & 0xFF;
+
+					if (r < Integer.valueOf(value1.getText())) {
+						r = r - Integer.valueOf(enhancement.getText()) < 0 ? 0 : r - Integer.valueOf(enhancement.getText());
+					} else if (r > Integer.valueOf(value2.getText())) {
+						r = r + Integer.valueOf(enhancement.getText()) > 255 ? 255 : r + Integer.valueOf(enhancement.getText());
+					}
+					if (g < Integer.valueOf(value1.getText())) {
+						g = g - Integer.valueOf(enhancement.getText()) < 0 ? 0 : g - Integer.valueOf(enhancement.getText());
+					} else if (g > Integer.valueOf(value2.getText())) {
+						g = g + Integer.valueOf(enhancement.getText()) > 255 ? 255 : g + Integer.valueOf(enhancement.getText());
+					}
+					if (b < Integer.valueOf(value1.getText())) {
+						b = b - Integer.valueOf(enhancement.getText()) < 0 ? 0 : b - Integer.valueOf(enhancement.getText());
+					} else if (b > Integer.valueOf(value2.getText())) {
+						b = b + Integer.valueOf(enhancement.getText()) > 255 ? 255 : b + Integer.valueOf(enhancement.getText());
+					}
+					Color color = new Color(r, g, b);
+					image.setRGB(x, y, color.getRGB());
 				}
 			}
 			System.out.println(value1.getText());
